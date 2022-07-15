7c8587fbefb95ceea16fa8aa2babbae8b1103fc7
Baguette-Enjoyer <78330575+Baguette-Enjoyer@users.noreply.github.com>
2022-07-15 12:20:35 +0700

    CSS App Indexjs

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import LogIn from "./login";
+import Test from "./test";
+import axios from "axios";
+
+function App() {
+  return <LogIn />;
+}
+
+export default App;
